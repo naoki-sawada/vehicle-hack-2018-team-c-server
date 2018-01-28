9cb46fe340fd94e7b324a56e4819412b5962c910
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-01-28 12:13:31 +0900

    add function of tire stress

--- index.js
+++ index.js
@@ -11,9 +11,52 @@ socket.on('message', (data) => {
 });
 
 const vias = new ViasClass();
+
+let distance_past = 0;
+let distance = 0;
+
+let break_pedal_past = false;
+let break_pedal = false;
+
+let stress_tire_past = 0;
+let stress_tire = 0;
+
 vias.doDisconnect();
 vias.doConnect();
 vias.on('message', (data) => {
-  // console.log(data);
-  // socket.send({ type: 'やったね' });
+
+    if (data.type === 'DISTANCE_TOTAL') {
+        if (distance_past === 0) {
+            distance_past = data.val;
+        }
+
+        distance = data.val;
+
+        if (distance > distance_past) {
+            stress_tire += 5;
+            distance_past = distance;
+        }
+    }
+
+    if (data.type === 'BRAKE_PEDAL') {
+        if (data.val === 'true' && break_pedal_past === false) {
+            stress_tire += 2;
+        }
+
+        break_pedal_past = data.val === 'true';
+    }
+
+    if (data.type === 'ACCEL_Y') {
+      if (data.val > 0.01) {
+          stress_tire += 1;
+      }
+    }
+
+    //
+    if (stress_tire_past < stress_tire) {
+        stress_tire_past = stress_tire;
+        socket.send({ type: 'tire_stress', val: stress_tire });
+    }
+
+    // socket.send({ type: 'やったね' });
 });

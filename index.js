01fb7cdf80f5def19ba35d38b310c9d589f4c4ec
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-01-28 13:03:43 +0900

    stress max function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ViasClass = require('./src/vias');
 const SocketIO = require('./src/SocketIO');
+const axios = require('./node_modules/axios');
 
 const socket = new SocketIO({
   logName: 'client',
@@ -20,6 +21,8 @@ let break_pedal = false;
 
 let stress_tire_past = 0;
 let stress_tire = 0;
+
+let isDrive = false;
 
 vias.doDisconnect();
 vias.doConnect();
@@ -56,6 +59,39 @@ vias.on('message', (data) => {
     if (stress_tire_past < stress_tire) {
         stress_tire_past = stress_tire;
         socket.send({ type: 'tire_stress', val: stress_tire });
+    }
+
+    // アラート出す
+    if (stress_tire === 37) {
+        socket.send({ type: 'tire_stress_max', val: true });
+    }
+
+    if (data.type === 'VEHICLE_SPEED') {
+        if (data.val > 0 && isDrive === false) {
+            isDrive = true;
+
+            axios.get('http://192.168.100.104:8080/test?d=1')
+                .then(function (response) {
+                    console.log('send!');
+                })
+                .catch(function (error) {
+                    console.log('connect error');
+                });
+
+        }
+
+        if (data.val < 1 && isDrive === true) {
+            isDrive = false;
+
+            axios.get('http://192.168.100.104:8080/test?d=0')
+                .then(function (response) {
+                    console.log('send!');
+                })
+                .catch(function (error) {
+                    console.log('connect error');
+                });
+
+        }
     }
 
     // socket.send({ type: 'やったね' });
